561ea048f95e1e4220e223479ec4fbad5e56f3f6
Tongjie <tjwang0221@gmail.com>
2019-05-06 14:46:22 -0700

    version 2

--- test/CalcBasicCommandTest.cpp
+++ test/CalcBasicCommandTest.cpp
@@ -3,229 +3,146 @@
 namespace {
     GTEST_TEST(CalcBasicCommandTest, handleZeroAndDecimalPointInput) {
         Calculator c = Calculator();
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDecimalPointInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.");
-        c.updateInputByCommand(CalcCommand::makeDecimalPointInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-        ASSERT_EQ(c.getCurrentInput(), "0.0");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-        ASSERT_EQ(c.getCurrentInput(), "0.00");
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Zero);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Zero);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::DecimalPoint);
+        ASSERT_EQ(c.getResult(), "0.");
+        c.makeInput(Calculator::Command::DecimalPoint);
+        ASSERT_EQ(c.getResult(), "0.");
+        c.makeInput(Calculator::Command::Zero);
+        ASSERT_EQ(c.getResult(), "0.0");
+        c.makeInput(Calculator::Command::Zero);
+        ASSERT_EQ(c.getResult(), "0.00");
     }
 
     GTEST_TEST(CalcBasicCommandTest, handleClearAndDelInput) {
         Calculator c = Calculator();
-        c.setCurrentInput("12.345");
-        ASSERT_EQ(c.getCurrentInput(), "12.345");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "12.34");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "12.3");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "12.");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "12");
-        c.updateInputByCommand(CalcCommand::makeClearInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeClearInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('1'));
-        ASSERT_EQ(c.getCurrentInput(), "1");
+        c.setInput("12.345");
+        ASSERT_EQ(c.getResult(), "12.345");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "12.34");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "12.3");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "12.");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "12");
+        c.makeInput(Calculator::Command::Clear);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Clear);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::One);
+        ASSERT_EQ(c.getResult(), "1");
     }
 
     GTEST_TEST(CalcBasicCommandTest, handlePercentInput) {
         Calculator c = Calculator();
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeDecimalPointInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("1");
-        ASSERT_EQ(c.getCurrentInput(), "1");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.01");
-        c.setCurrentInput("10");
-        ASSERT_EQ(c.getCurrentInput(), "10");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.1");
-        c.setCurrentInput("200");
-        ASSERT_EQ(c.getCurrentInput(), "200");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "2");
-        c.setCurrentInput("201");
-        ASSERT_EQ(c.getCurrentInput(), "201");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "2.01");
-        c.setCurrentInput("15");
-        ASSERT_EQ(c.getCurrentInput(), "15");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.15");
-        c.setCurrentInput("7652");
-        ASSERT_EQ(c.getCurrentInput(), "7652");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "76.52");
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::DecimalPoint);
+        ASSERT_EQ(c.getResult(), "0.");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "0");
+        c.setInput("1");
+        ASSERT_EQ(c.getResult(), "1");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "0.01");
+        c.setInput("10");
+        ASSERT_EQ(c.getResult(), "10");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "0.1");
+        c.setInput("200");
+        ASSERT_EQ(c.getResult(), "200");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "2");
+        c.setInput("201");
+        ASSERT_EQ(c.getResult(), "201");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "2.01");
+        c.setInput("15");
+        ASSERT_EQ(c.getResult(), "15");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "0.15");
+        c.setInput("7652");
+        ASSERT_EQ(c.getResult(), "7652");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "76.52");
     }
 
     GTEST_TEST(CalcBasicCommandTest, handleNegation) {
         Calculator c = Calculator();
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeClearInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("12345");
-        ASSERT_EQ(c.getCurrentInput(), "12345");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "-12345");
-        c.updateInputByCommand(CalcCommand::makePercentInput());
-        ASSERT_EQ(c.getCurrentInput(), "-123.45");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "123.45");
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "-0");
+        c.makeInput(Calculator::Command::Clear);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "-0");
+        c.makeInput(Calculator::Command::Delete);
+        ASSERT_EQ(c.getResult(), "0");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "-0");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "0");
+        c.setInput("12345");
+        ASSERT_EQ(c.getResult(), "12345");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "-12345");
+        c.makeInput(Calculator::Command::Percent);
+        ASSERT_EQ(c.getResult(), "-123.45");
+        c.makeInput(Calculator::Command::Negate);
+        ASSERT_EQ(c.getResult(), "123.45");
     }
 
     GTEST_TEST(CalcBasicCommandTest, handleAddition) {
         Calculator c{};
-        c.setCurrentInput("123");
-        ASSERT_EQ(c.getCurrentInput(), "123");
-        c.updateInputByCommand(CalcCommand::makePlusInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("666");
-        ASSERT_EQ(c.getCurrentInput(), "666");
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(c.getCurrentInput(), "789");
-        c.updateInputByCommand(CalcCommand::makePlusInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("-45.67");
-        ASSERT_EQ(c.getCurrentInput(), "-45.67");
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(c.getCurrentInput(), "743.33");
+        c.setInput("123");
+        ASSERT_EQ(c.getResult(), "123");
+        c.makeInput(Calculator::Command::Plus);
+        ASSERT_EQ(c.getResult(), "0");
+        c.setInput("666");
+        ASSERT_EQ(c.getResult(), "666");
+        c.makeInput(Calculator::Command::Eval);
+        ASSERT_EQ(c.getResult(), "789");
+        c.makeInput(Calculator::Command::Plus);
+        ASSERT_EQ(c.getResult(), "0");
+        c.setInput("-45.67");
+        ASSERT_EQ(c.getResult(), "-45.67");
+        c.makeInput(Calculator::Command::Eval);
+        ASSERT_EQ(c.getResult(), "743.33");
     }
 
     GTEST_TEST(CalcBasicCommandTest, handleMultiplePlusInput) {
         Calculator c{};
-        c.setCurrentInput("123");
-        c.updateInputByCommand(CalcCommand::makePlusInput());
-        c.setCurrentInput("234");
-        c.updateInputByCommand(CalcCommand::makePlusInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("233");
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(std::stoi(c.getCurrentInput()), 123 + 234 + 233);
+        c.setInput("123");
+        c.makeInput(Calculator::Command::Plus);
+        c.setInput("234");
+        c.makeInput(Calculator::Command::Plus);
+        ASSERT_EQ(c.getResult(), "0");
+        c.setInput("233");
+        c.makeInput(Calculator::Command::Eval);
+        ASSERT_EQ(std::stoi(c.getResult()), 123 + 234 + 233);
     }
 
     GTEST_TEST(CalcBasicCommandTest, testCaseForComplexInput) {
         Calculator c{};
-        c.setCurrentInput("233");
-        c.updateInputByCommand(CalcCommand::makeMinusInput());
-        c.setCurrentInput("234");
-        c.updateInputByCommand(CalcCommand::makeMultiplyInput());
-        c.setCurrentInput("3");
-        c.updateInputByCommand(CalcCommand::makePlusInput());
-        c.setCurrentInput("4");
-        c.updateInputByCommand(CalcCommand::makeDecimalPointInput());
-        c.updateInputByCommand(CalcCommand::makeDigitInput('5'));
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(c.getCurrentInput(), "1.5");  // (233 - 234) * 3 + 4.5 == 1.5
+        c.setInput("233");
+        c.makeInput(Calculator::Command::Minus);
+        c.setInput("234");
+        c.makeInput(Calculator::Command::Multiply);
+        c.setInput("3");
+        c.makeInput(Calculator::Command::Plus);
+        c.setInput("4");
+        c.makeInput(Calculator::Command::DecimalPoint);
+        c.makeInput(Calculator::Command::Five);
+        c.makeInput(Calculator::Command::Eval);
+        ASSERT_EQ(c.getResult(), "1.5");  // (233 - 234) * 3 + 4.5 == 1.5
     }
 
-    GTEST_TEST(CalcBasicCommandTest, handleDivisionAndReset) {
-        Calculator c{};
-        c.setCurrentInput("24");
-        c.updateInputByCommand(CalcCommand::makeDiviseInput());
-        c.setCurrentInput("8");
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(c.getCurrentInput(), "3");
-        c.updateInputByCommand(CalcCommand::makeResetInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.setCurrentInput("1");
-        c.updateInputByCommand(CalcCommand::makeDiviseInput());
-        c.setCurrentInput("3");
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        {
-            std::stringstream ss;
-            ss << 1.0 / 3.0;
-            ASSERT_EQ(c.getCurrentInput(), ss.str());
-        }
-        c.updateInputByCommand(CalcCommand::makeResetInput());
-        c.setCurrentInput("2");
-        c.updateInputByCommand(CalcCommand::makeDiviseInput());
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        {
-            std::stringstream ss;
-            ss << 2.0 / 0.0;
-            ASSERT_EQ(c.getCurrentInput(), ss.str());
-        }
-    }
-
-    GTEST_TEST(CalcBasicCommandTest, extraTestCase) {
-        Calculator c{};
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        c.updateInputByCommand(CalcCommand::makeEvalInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeResetInput());
-        c.setCurrentInput("-0.3");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "0.3");
-        c.setCurrentInput("-0.3");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0.");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeResetInput());
-        c.setCurrentInput("-13");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "-1");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeDelInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-    }
-
-    GTEST_TEST(CalcBasicCommandTest, extraTestCase2) {
-        Calculator c{};
-        c.updateInputByCommand(CalcCommand::makeMinusInput());
-        ASSERT_EQ(c.getCurrentInput(), "0");
-        c.updateInputByCommand(CalcCommand::makeNegateInput());
-        ASSERT_EQ(c.getCurrentInput(), "-0");
-        c.updateInputByCommand(CalcCommand::makeDigitInput('3'));
-        ASSERT_EQ(c.getCurrentInput(), "-3");
-    }
-
-    GTEST_TEST(CalcBasicCommandTest, extraTestCase3) {
-        {
-            Calculator c{};
-            c.updateInputByCommand(CalcCommand::makeDigitInput('1'));
-            ASSERT_EQ(c.getCurrentInput(), "1");
-            c.updateInputByCommand(CalcCommand::makeDigitInput('3'));
-            ASSERT_EQ(c.getCurrentInput(), "13");
-        }
-
-        {
-            Calculator c{};
-            c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-            ASSERT_EQ(c.getCurrentInput(), "0");
-            c.updateInputByCommand(CalcCommand::makeDigitInput('0'));
-            ASSERT_EQ(c.getCurrentInput(), "0");
-        }
-    }
 }
